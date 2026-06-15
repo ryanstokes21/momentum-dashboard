0e7cb16a8cc00f9f57104c210109097d68668958
Ryan Stokes <ryanstokescfbre@gmail.com>
2026-06-14 20:49:39 -0500

    add logic to apply active class for pathname

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-import { testFinance } from './finance.js';
-import { testFitness } from './fitness.js';
-import { testHabits } from './habits.js';
-import { testTask } from './task.js';
+const currentPage = window.location.pathname.split('/').pop();
+const links = document.querySelectorAll('.nav-list a');
+
+links.forEach((link) => {
+  if (link.getAttribute('href').includes(currentPage)) {
+    link.classList.add('active');
+  }
+});
